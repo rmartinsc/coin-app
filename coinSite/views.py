8527c98322d2189b393ab68461bef437907ccdc5
RodolphoCavalcanti <rodomart@iu.edu>
2015-08-11 14:15:36 -0400

    Added single post view and base template.

--- coinSite/views.py
+++ coinSite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Story
 
@@ -6,3 +6,7 @@ from .models import Story
 def post_list(request):
     posts = Story.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'coinSite/post_list.html', {'posts': posts})
+    
+def post_detail(request, pk):
+    post = get_object_or_404(Story, pk=pk)
+    return render(request, 'coinSite/post_detail.html', {'post': post})

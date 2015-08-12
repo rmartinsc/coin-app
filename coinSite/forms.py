7fd8246f0adfdadf5d48b1ac56c99bb102d4d69a
RodolphoCavalcanti <rodomart@iu.edu>
2015-08-12 09:34:37 -0400

    Improved UI for forms, list and buttons.

--- coinSite/forms.py
+++ coinSite/forms.py
@@ -1,9 +1,34 @@
 from django import forms
 
+from django.forms.widgets import RadioFieldRenderer
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+
+class MyCustomRenderer( RadioFieldRenderer ):
+    def render( self ):
+        """Outputs a series of <td></td> fields for this set of radio fields."""
+        return mark_safe(u'\n'.join([u'<td>%s</td>\n' % w for w in self]))
+
 from .models import Story
 
 class SubmitForm(forms.ModelForm):
 
     class Meta:
+        
+        BUSINESS = [('Aviation', 'Aviation'),
+                    ('Corporate', 'Corporate'),
+                    ('Energy Management', 'Energy Management'),
+                    ('Power and Water', 'Power and Water'),
+                    ('Oil and Gas', 'Oil and Gas'),
+                    ('Healthcare', 'Healthcare'),
+                    ('Transportation', 'Transportation'),
+                    ]
+        
         model = Story
         fields = ('coinReceiverName', 'receiverBusiness', 'storyTitle', 'storyText',)
+        widgets = {
+                   'storyText' : forms.Textarea(attrs={'cols': 80, 'rows': 20}),
+                   'receiverBusiness' : forms.RadioSelect(choices = BUSINESS,
+                                                          renderer=MyCustomRenderer,
+                                                          ),
+                   }
--- coinSite/views.py
+++ coinSite/views.py
@@ -7,7 +7,7 @@ from .models import Story
 
 # Create your views here.
 def post_list(request):
-    posts = Story.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Story.objects.filter(published_date__lte=timezone.now()).order_by('receiverBusiness')
     return render(request, 'coinSite/post_list.html', {'posts': posts})
     
 def post_detail(request, pk):
